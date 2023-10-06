b61f81506ce134c776a3daecd0e9ef11091ff1c3
Keli06001 <keli06001@utb.vaxjo.se>
2023-10-06 13:15:33 +0200

    Add project files.

---
+++ Uppgift3.3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace uppgift3_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur många timmar vill du hyra bilen?");
+            int timmar=int.Parse(Console.ReadLine());
+            int pris = (timmar * 80);
+            if (timmar < 2) 
+            {
+                Console.WriteLine("Du kan inte hyra bilen så kort tid");
+            }
+            if (pris > 950)
+            {
+                Console.WriteLine("Det kommer att kosta dig 950kr");
+            }
+            else 
+            {
+                Console.WriteLine("Det kommer att kosta dig "+pris+"kr");
+            }
+            Console.ReadKey();
+        }
+    }
+}
